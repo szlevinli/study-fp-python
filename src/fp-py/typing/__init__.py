9976af9ffe3a34feaae4466b33dd3d3738a768c1
levin li <3259963@qq.com>
2022-09-15 07:27:53 +0800

    reexport Monad and Monoid

--- src/fp-py/typing/__init__.py
+++ src/fp-py/typing/__init__.py
@@ -1,2 +1,4 @@
-from .functor import Functor as Functor
 from .applicative import Applicative as Applicative
+from .functor import Functor as Functor
+from .monad import Monad as Monad
+from .monoid import Monoid as Monoid
